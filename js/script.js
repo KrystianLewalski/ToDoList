321fe97d52924eee307dbcdc42e03e2557afd980
Krystian Lewalski <Krystian.Lewalski@interia.pl>
2023-01-13 19:32:33 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,24 +10,39 @@
         },
     ];
 
+    const addnewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li>
+            <button class="js-remove">usuń</button>
             ${task.content}
             </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addnewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove")
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-        render();
     };
 
     const onFormSubmit = (event) => {
